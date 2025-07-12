e3985cb4c5455dfd6cc5665c0411ac5be77d8296
ag31415 <averie90210@gmail.com>
2025-07-12 14:34:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -64,5 +64,8 @@ def index():
   with open('index.html', 'r') as f:
     return f.read()
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
